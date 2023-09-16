8acc7fb2479c046e182121c62eb5077425a08c82
Raj Kiran Chaudhary <raj.chaudhary@ebpearls.com>
2023-09-16 13:00:51 +0545

    project initialized

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import { useState } from "react";
+import "./App.css";
+import UserCard from "./component/UserCard";
+
+function App() {
+  return <UserCard />;
+}
+
+export default App;
---
+++ src/component/UserCard.jsx
@@ -0,0 +1,45 @@
+import React, { useState } from "react";
+import userData from "../data";
+
+import ChevronLeftIcon from "@mui/icons-material/ChevronLeft";
+import ChevronRightIcon from "@mui/icons-material/ChevronRight";
+
+function UserCard() {
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  const showPrev = () => {
+    setCurrentIndex((prev) => prev - 1);
+  };
+
+  const showNext = () => {
+    setCurrentIndex((next) => next + 1);
+  };
+
+  const surpriseBtn = () => {
+    const surpriseValue = Math.floor(Math.random() * userData.length);
+    setCurrentIndex(currentIndex[surpriseValue]);
+    console.log(surpriseValue);
+  };
+
+  return (
+    <div className="wrapper">
+      <img src={userData[currentIndex].img} alt={userData[currentIndex].name} />
+      <h3>{userData[currentIndex].name}</h3>
+      <p>{userData[currentIndex].designation}</p>
+      <p>{userData[currentIndex].description}</p>
+      <div>
+        <button className="prev" onClick={showPrev}>
+          <ChevronLeftIcon />
+        </button>
+        <button className="next" onClick={showNext}>
+          <ChevronRightIcon />
+        </button>
+      </div>
+      <button className="surprise-btn" onClick={surpriseBtn}>
+        Surprise me
+      </button>
+    </div>
+  );
+}
+
+export default UserCard;
